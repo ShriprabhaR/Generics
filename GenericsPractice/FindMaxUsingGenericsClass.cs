892fd482d101e5cb6a42315942962554ce57f3be
ShriprabhaR <raichurprabha1@gmail.com>
2024-12-16 17:23:51 +0530

    All commits

---
+++ GenericsPractice/FindMaxUsingGenericsClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsPractice
+{
+    internal class FindMaxUsingGenericsClass<T> where T : IComparable<T>
+
+    {
+        private T value1;
+        private T value2;
+        private T value3;
+
+        public FindMaxUsingGenericsClass(T value1, T value2, T value3)
+        {
+            this.value1 = value1;
+            this.value2 = value2;
+            this.value3 = value3;
+        }
+
+        public T FindMax()
+        {
+
+            if (value1.CompareTo(value2) > 0 && value1.CompareTo(value3) > 0)
+            {
+                return value1;
+            }
+            else if (value2.CompareTo(value3) > 0 && value2.CompareTo(value1) > 0)
+            {
+                return value2;
+            }
+            else
+            {
+                return value3;
+            }
+
+        }
+    }
+}
